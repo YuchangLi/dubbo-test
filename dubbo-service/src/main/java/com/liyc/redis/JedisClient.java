175bff80e08ab25ba459c57073eedac1587a67c5
li_yc <648821884@qq.com>
2017-03-24 20:26:46 +0800

    cm

--- dubbo-service/src/main/java/com/liyc/redis/JedisClient.java
+++ dubbo-service/src/main/java/com/liyc/redis/JedisClient.java
@@ -1,20 +1,85 @@
 package com.liyc.redis;
 
-import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 import redis.clients.jedis.Jedis;
 
 public class JedisClient {
-	public static void main(String[] args) throws IOException {
-		 Jedis jedis = new Jedis("123.56.216.146"); 
-     System.out.println("Connection to server sucessfully"); 
-     //check whether server is running or not 
-     jedis.auth("root");
-     System.out.println("Server is running: "+jedis.ping()); 
-     jedis.keys("*").forEach(key -> System.out.println(key));
-     System.out.println(jedis.get("name"));
-     jedis.lrange("list", 0, -1).forEach(key -> System.out.println(key));
-     jedis.pfcount("hyperlogIp");
-     System.in.read(); // 按任意键退出
-	}
+  private Jedis jedis ; 
+  
+  @Before
+  public void before(){
+    jedis = new Jedis("123.56.216.146");
+    jedis.auth("root");
+  }
+  
+  //string
+  @Test
+  public void testStr(){
+    //获取
+    String strName = jedis.get("name");
+    System.out.println("strName = "+strName);
+    //更新
+    String result = jedis.set("names", strName+"_");
+    System.out.println("result = "+result);
+    result = jedis.set("names", "newname", "xx", "EX", 10l);
+    System.out.println("setNx result = "+result);
+  }
+  
+  //list
+  @Test
+  public void testList(){
+    //获取
+    List<String> list = jedis.lrange("list", 0l, -1l);
+    System.out.println("list.class = "+list.getClass());
+    list.forEach(System.out::println);
+    //添加 返回index
+    Long index = jedis.rpush("list", "map");
+    System.out.println("rpush index = "+index);
+    list = jedis.lrange("list", 0l, -1l);
+    list.forEach(System.out::println);
+    //删除
+    String delResult = jedis.rpop("list");
+    System.out.println("delResult = "+delResult);
+    list = jedis.lrange("list", 0l, -1l);
+    list.forEach(System.out::println);
+  }
+  
+  //set
+  @Test
+  public void testSet(){
+    //获取
+    Set<String> set = jedis.smembers("set");
+    System.out.println("set.class = "+set.getClass()+" set.size = "+set.size());
+    set.forEach(System.out::println);
+    //添加 1成功 0失败
+    Long result = jedis.sadd("set", "sss");
+    System.out.println("sadd result = "+result);
+    //删除
+    result = jedis.srem("set", "sss");
+    System.out.println("srem result = "+result);
+  }
+  
+  //zset
+  @Test
+  public void testZSet(){
+    //获取
+    Set<String> zset = jedis.zrange("zset", 0, -1);
+    System.out.println("zset.class = "+zset.getClass()+" zset.size = "+zset.size());
+    zset.forEach(System.out::println);
+//    jedis.zadd
+    //带
+    jedis.zrangeWithScores("zset", 0, -1);
+  }
+  
+  
+  @After
+  public void after(){
+    jedis.close();
+  }
 }
